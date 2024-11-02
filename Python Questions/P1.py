ecba4070782705cf94907df60e4ba8f907cfab2a
Saksham <65125184+Victor-00@users.noreply.github.com>
2024-11-02 13:44:21 +0530

    Create P1.py

---
+++ Python Questions/P1.py
@@ -0,0 +1,8 @@
+def triple_and(a,b,c):
+    if(a==True and b==True) and (a== True and c== True):
+        return True
+    else:
+        return False
+
+result = triple_and(True,True,True)
+print(result)
