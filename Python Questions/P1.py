ffaece4adb9293805886aaf3692ef9b8964984fc
Saksham <65125184+Victor-00@users.noreply.github.com>
2024-11-05 13:09:27 +0530

    Update P1.py
    Define a function named “triple_and” that takes three parameters and
    returns True only if they are all True and False otherwise

--- Python Questions/P1.py
+++ Python Questions/P1.py
@@ -1,3 +1,8 @@
+'''
+Define a function named “triple_and” that takes three parameters and
+returns True only if they are all True and False otherwise
+'''
+
 def triple_and(a,b,c):
     if(a==True and b==True) and (a== True and c== True):
         return True
