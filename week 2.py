fe67c74046ea5abef885a9732be13058a9b0d22e
Saksham <65125184+Victor-00@users.noreply.github.com>
2024-11-06 15:32:56 +0530

    Update week 2.py

--- week 2.py
+++ week 2.py
@@ -16,3 +16,13 @@ maxT = df['toss_winner'].value_counts().idxmax()
 print("Q4)- The team with most toss wins is : " + maxT)
 minT = df['toss_winner'].value_counts().idxmin()
 print("Q4) - The team with least toss wins is : " + minT)
+'''
+question 5
+'''
+value1_counts = df['result'].value_counts()
+print("Q6) - Total count of matches :-")
+for value1, count1 in value1_counts.items():
+    if value1 != "no result":
+      print(f"{value1}: {count1}")
+print("Q7)- Team names where match resulted in a tie :- ")
+print(df.loc[df['result'] == "tie", ['team1', 'team2']])
