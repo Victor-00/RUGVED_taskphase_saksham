b5742927255e8262a077e700adcfd3ebcdbbd3e1
Saksham <65125184+Victor-00@users.noreply.github.com>
2024-11-06 15:05:42 +0530

    Update p9.py

--- Python Questions/p9.py
+++ Python Questions/p9.py
@@ -2,16 +2,16 @@
 Write a python function to encrypt a string using Ceasar’s Cipher
 '''
 
-def cc(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            encrypted_text += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
-        else:
-            encrypted_text += char  
-    return encrypted_text
+a = "abcdefghijklmnopqrstuvwxyz"
+r = ""
 
-text = input("Enter the string : ")
-shift = int(input("Enter the shift : "))
-result = cc(text,shift)
-print(result)
+txt = input("Enter the string: ")
+s = int(input("Enter the shift: "))
+
+for c in txt:
+    if c in a:
+        r = r + a[(a.index(c) + s) % 26]
+    else:
+        r = r + c
+
+print(r)
